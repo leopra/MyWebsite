5db469eb4ade0f7e63c49319190bae23737c36f3
leopra <leonardo.pratesi@studenti.unipd.it>
2020-08-06 14:24:10 +0200

    fix images

--- app/components/Projects/Cell.js
+++ app/components/Projects/Cell.js
@@ -11,7 +11,7 @@ const Cell = ({ data }) => (
         <time className="published">{dayjs(data.date).format('MMMM, YYYY')}</time>
       </header>
       <a href={data.link} className="image">
-        <img src={url.resolve(BASE_PATH, data.image)} alt={data.title} />
+        <img src={`${BASE_PATH}` + data.image} alt={data.title} />
       </a>
       <div className="description">
         <p>{data.desc}</p>
--- app/views/Projects.js
+++ app/views/Projects.js
@@ -14,7 +14,7 @@ const Projects = () => (
       <header>
         <div className="title">
           <h2><Link to="/projects">Projects</Link></h2>
-          <p>A selection of projects that I made recently&apos;</p>
+          <p>A selection of projects that I made recently</p>
         </div>
       </header>
       {data.map((project) => (
