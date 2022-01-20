48ff2ba846ac5c6d4fa83d7f40675d3a08f5c353
leopra <L.Pratesi@almaviva.it>
2022-01-20 14:12:17 +0100

    updated positions

--- app/data/resume/positions.js
+++ app/data/resume/positions.js
@@ -10,6 +10,28 @@ const positions = [{
     'Write understandable documentation of the APIs',
   ],
 },
+{
+  company: 'Eutopia Green',
+  position: 'Data Scientist',
+  link: 'https://www.eutopiagreen.com/',
+  daterange: 'October 2020 - June 2021',
+  points: [
+    'Developing Neural Networks and Word Embeddings for Text Categorization Task',
+    'Building the pipeline responsible for data classification and writing documentation',
+    'Improved initial company text classifier accuracy by 10%'
+  ],
+},
+{
+  company: 'Almawave',
+  position: 'Testing Engineer',
+  link: 'https://www.almawave.com/en/',
+  daterange: 'September 2021 - Present',
+  points: [
+    'Developing a Machine Learning platform for NLP models: NER, Classification, Conversational AI.',
+    'Designing training pipelines using Cloud and custom-made microservices',
+    'Tech stack: Docker, Tensorflow, Kubeflow, Tranformers, Pytorch, Huggingface',
+  ],
+}
 ];
 
 export default positions;
